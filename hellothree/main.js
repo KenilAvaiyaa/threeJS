d284d9fe4909dd0136098c2eee48c09224872ca9
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-09-23 15:44:20 -0400

    learn orbitcontrol

--- hellothree/main.js
+++ hellothree/main.js
@@ -1,10 +1,11 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00,wireframe: true } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -14,6 +15,11 @@ const canvas = document.querySelector('canvas');
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 4.5;
+
 window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -22,8 +28,10 @@ window.addEventListener('resize', () => {
 
 function animate() {
 	window.requestAnimationFrame(animate);
-  renderer.render(scene, camera);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+  controls.update();
+  
 }
 animate();
